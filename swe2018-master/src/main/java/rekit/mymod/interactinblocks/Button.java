270a0953a691f61119e8d5d6fc36d18d0ddb9e98
Grégoire Mercier <g.mercier@protonmail.com>
2018-07-13 19:47:01 +0200

    implemented Listener

--- swe2018-master/src/main/java/rekit/mymod/interactinblocks/Button.java
+++ swe2018-master/src/main/java/rekit/mymod/interactinblocks/Button.java
@@ -1,16 +1,7 @@
 package rekit.mymod.interactinblocks;
 
-public class Button implements Listener {
+public abstract class Button extends Listener {
 	
-	Reciever r;
 	
-	public Button() {
-		
-	}
-	
-	@Override
-	public void act(Reciever v) {
-		
-	}
 	
 }
--- swe2018-master/src/main/java/rekit/mymod/interactinblocks/Listener.java
+++ swe2018-master/src/main/java/rekit/mymod/interactinblocks/Listener.java
@@ -1,7 +1,32 @@
 package rekit.mymod.interactinblocks;
 
-public interface Listener {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Listener {
 	
-	public void act(Reciever a);
+	protected List<Reciever> recievers;
+	
+	public Listener() {
+		recievers = new ArrayList<Reciever>();
+	}
+	
+	public void act(Reciever a) {
+		for(Reciever i: recievers) {
+			i.performAction();
+		}
+	}
+	
+	public void addReciever(Reciever r) {
+		recievers.add(r);
+	}
+	
+	public void removeReciever(Reciever r) {
+		recievers.remove(r);
+	}
+	
+	public void removeAll(Reciever r) {
+		recievers = new ArrayList<Reciever>();
+	}
 
 }
